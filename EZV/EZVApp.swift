0563d7f4d0b1993bcd80681cfcbd7ba74fdf2045
rarito83 <rarito.jogja@gmail.com>
2023-03-03 13:13:01 +0700

    Initial Commit

---
+++ EZV/EZVApp.swift
@@ -0,0 +1,17 @@
+//
+//  EZVApp.swift
+//  EZV
+//
+//  Created by Rarito on 03/03/23.
+//
+
+import SwiftUI
+
+@main
+struct EZVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
